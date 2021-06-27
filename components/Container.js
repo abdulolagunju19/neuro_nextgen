3864fab8249bc685f6d66479c20f94392a4ebb11
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-06-27 14:06:18 -0600

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -59,7 +59,7 @@ const Container = ({ children }) => {
                             Home
                         </Button>
                     </NextLink>
-                    <NextLink href="../pages/stat_tutorial.mdx" passHref>
+                    <NextLink href="/" passHref>
                         <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
                             Simplistic Statistics
                         </Button>
