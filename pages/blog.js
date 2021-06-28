506e6020b12e35a494f594cbb236b3d4ee4ac5f5
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-06-27 23:31:45 -0600

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -28,7 +28,7 @@ export default function Blog({ posts }) {
     return (
         <>
             <Head>
-                <title>Blog! - Abdul-Samad Olagunju</title>
+                <title>Blog - Abdul-Samad Olagunju</title>
             </Head>
             <Container>
                 <Stack
