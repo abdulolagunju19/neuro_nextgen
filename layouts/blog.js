e216c5ec6215ab4e2036b455b24d6356fe9ef590
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-06-27 23:33:22 -0600

    Update blog.js

--- layouts/blog.js
+++ layouts/blog.js
@@ -24,7 +24,7 @@ export default function BlogLayout({ children, frontMatter }) {
     return (
         <Container>
             <Head>
-                <title>${slug} - Blog - AbdulSamad Olagunju</title>
+                <title>${slug} - Blog - Abdul_Samad Olagunju</title>
             </Head>
             <Stack
                 as="article"
