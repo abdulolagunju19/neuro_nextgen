50626213cb399941841b5bb8ae41575a8d64de80
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-06-28 22:09:37 -0600

    Update SimpleStatsPost.js

--- components/SimpleStatsPost.js
+++ components/SimpleStatsPost.js
@@ -11,7 +11,7 @@ const SimpleStatsPost = ({ title, publishedAt, summary, slug }) => {
     }
 
     return (
-        <NextLink href={`simplestats/${slug}`} passHref>
+        <NextLink href={`simplestats/pythonbot.mdx`} passHref>
             <Link w="100%" _hover={{ textDecoration: 'none' }}>
                 <Box mb={10} display="block" width="100%">
                     <Flex
