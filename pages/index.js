37865abb37709b5b24779a298a7fd709b55ad13e
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-04 21:38:07 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -50,31 +50,6 @@ export default function Index() {
             className="next-image"
           />
           <br />
-          <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
-            <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
-            <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
-              <div className="flex flex-col space-y-4">
-                <Link href="/">
-                  <a className="text-gray-500 hover:text-gray-600 transition">Home</a>
-                </Link>
-                <Link href="/simplestats">
-                  <a className="text-gray-500 hover:text-gray-600 transition">
-                    Simplistic Statistics
-                  </a>
-                </Link>
-                <Link href="/blog">
-                  <a className="text-gray-500 hover:text-gray-600 transition">
-                    Blog
-                  </a>
-                </Link>
-                <Link href="/https://github.com/abdulolagunju19">
-                  <a className="text-gray-500 hover:text-gray-600 transition">
-                    Github
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </footer>
         </Flex>
       </Stack>
     </Container>
