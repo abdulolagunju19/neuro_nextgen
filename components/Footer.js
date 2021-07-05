0f08e6d36ba3738441a515575899c3d3dfb08656
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-05 17:58:24 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,48 +1,116 @@
-import Link from 'next/link';
-import {
-  useColorMode,
-  Heading,
-  Text,
-  Flex,
-  Stack
-} from '@chakra-ui/react'
+import React from 'react'
+import NextLink from 'next/link'
+import { Flex, Link, IconButton, useColorMode } from '@chakra-ui/react'
+import { FiGithub, FiTwitter, FiLinkedin, FiYoutube, FiMail } from "react-icons/fi"
 
-const ExternalLink = ({ href, children }) => (
-  <a
-    className="text-gray-500 hover:text-gray-600 transition"
-    target="_blank"
-    rel="noopener noreferrer"
-    href={href}
-  >
-    {children}
-  </a>
-);
+export const Footer = () => {
 
-export default function Footer() {
-  return (
-    <footer>
-      <hr/>
-      <div>
-        <div>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/">
-            <a>
-              About
-            </a>
-          </Link>
-          <Link href="/">
-            <a>
-              Newsletter
-            </a>
-          </Link>
-          <ExternalLink href="https://twitter.com">
-            Twitter
-          </ExternalLink>
-          <ExternalLink href="https://github.com/abdulolagunju19">GitHub</ExternalLink>
-        </div>
-      </div>
-    </footer>
-  );
+    const { colorMode } = useColorMode()
+    const borderIcon = {
+        light: 'gray.400',
+        dark: 'gray.500'
+    }
+    const footerHoverBg = {
+        light: 'gray.100',
+        dark: 'gray.700',
+    }
+    return (
+        <Flex align="center" mb={4} direction="column" visibility={'hidden', 'visible', 'visible'} display={['none', 'flex', 'flex']}>
+            <div>
+                <Link href="https://twitter.com" title="Twitter" isExternal>
+                    <IconButton
+                        aria-label="Twitter"
+                        icon={<FiTwitter />}
+                        size="lg"
+                        color={borderIcon[colorMode]}
+                        variant="ghost"
+                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
+                    />
+                </Link>
+                <Link href="https://github.com/abdulolagunju19" title="GitHub" isExternal>
+                    <IconButton
+                        aria-label="GitHub"
+                        icon={<FiGithub />}
+                        size="lg"
+                        color={borderIcon[colorMode]}
+                        variant="ghost"
+                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
+                    />
+                </Link>
+                <Link
+                    href="https://www.linkedin.com"
+                    title="LinkedIn"
+                    isExternal
+                >
+                    <IconButton
+                        aria-label="LinkedIn"
+                        icon={<FiLinkedin />}
+                        size="lg"
+                        color={borderIcon[colorMode]}
+                        variant="ghost"
+                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
+                    />
+                </Link>
+                <Link
+                    href="https://www.youtube.com"
+                    title="YouTube"
+                    isExternal
+                >
+                    <IconButton
+                        aria-label="YouTube"
+                        icon={<FiYoutube />}
+                        size="lg"
+                        color={borderIcon[colorMode]}
+                        variant="ghost"
+                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
+                    />
+                </Link>
+                <Link href="mailto:abdulolagunju19@gmail.com" title="Email" isExternal>
+                    <IconButton
+                        aria-label="Email"
+                        icon={<FiMail />}
+                        size="lg"
+                        color={borderIcon[colorMode]}
+                        variant="ghost"
+                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
+                    />
+                </Link>
+            </div>
+            <div>
+                <NextLink href="/blog" passHref>
+                    <Link
+                        fontSize="md"
+                        color="gray.500"
+                        minWidth="100px"
+                        mr={2}
+                        title="Blog"
+                    >
+                        /hello
+                    </Link>
+                </NextLink>
+                <NextLink href="/simplestats" passHref>
+                    <Link
+                        fontSize="md"
+                        color="gray.500"
+                        minWidth="100px"
+                        mr={2}
+                        title="Simplistic Statistics"
+                    >
+                        /videos
+                    </Link>
+                </NextLink>
+                <NextLink href="/contactme" passHref>
+                    <Link
+                        fontSize="md"
+                        color="gray.500"
+                        minWidth="100px"
+                        mr={2}
+                        title="Contact Me"
+                    >
+                        /videos
+                    </Link>
+                </NextLink>
+            </div>
+        </Flex>
+    )
 }
