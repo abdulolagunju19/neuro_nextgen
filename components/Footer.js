fdc23c325c80609085174ce488e2c64dfdebaf0e
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-05 18:02:06 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NextLink from 'next/link'
 import { Flex, Link, IconButton, useColorMode } from '@chakra-ui/react'
-import { FiGithub, FiTwitter, FiLinkedin, FiYoutube, FiMail } from "react-icons/fi"
+
 
 export const Footer = () => {
 
@@ -17,63 +17,9 @@ export const Footer = () => {
     return (
         <Flex align="center" mb={4} direction="column" visibility={'hidden', 'visible', 'visible'} display={['none', 'flex', 'flex']}>
             <div>
-                <Link href="https://twitter.com" title="Twitter" isExternal>
-                    <IconButton
-                        aria-label="Twitter"
-                        icon={<FiTwitter />}
-                        size="lg"
-                        color={borderIcon[colorMode]}
-                        variant="ghost"
-                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
-                    />
-                </Link>
                 <Link href="https://github.com/abdulolagunju19" title="GitHub" isExternal>
-                    <IconButton
-                        aria-label="GitHub"
-                        icon={<FiGithub />}
-                        size="lg"
-                        color={borderIcon[colorMode]}
-                        variant="ghost"
-                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
-                    />
-                </Link>
-                <Link
-                    href="https://www.linkedin.com"
-                    title="LinkedIn"
-                    isExternal
-                >
-                    <IconButton
-                        aria-label="LinkedIn"
-                        icon={<FiLinkedin />}
-                        size="lg"
-                        color={borderIcon[colorMode]}
-                        variant="ghost"
-                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
-                    />
-                </Link>
-                <Link
-                    href="https://www.youtube.com"
-                    title="YouTube"
-                    isExternal
-                >
-                    <IconButton
-                        aria-label="YouTube"
-                        icon={<FiYoutube />}
-                        size="lg"
-                        color={borderIcon[colorMode]}
-                        variant="ghost"
-                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
-                    />
                 </Link>
                 <Link href="mailto:abdulolagunju19@gmail.com" title="Email" isExternal>
-                    <IconButton
-                        aria-label="Email"
-                        icon={<FiMail />}
-                        size="lg"
-                        color={borderIcon[colorMode]}
-                        variant="ghost"
-                        _hover={{ backgroundColor: footerHoverBg[colorMode] }}
-                    />
                 </Link>
             </div>
             <div>
