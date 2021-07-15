15cd148bdc0836d0989553150722703b6dc12707
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-14 18:57:51 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -18,6 +18,7 @@ export const Footer = () => {
     return (
         <Flex align="center" mb={4} direction="column" visibility={'hidden', 'visible', 'visible'} display={['none', 'flex', 'flex']}>
             <div>
+                <Divider mb={12} mt={16} w='90%' alignSelf="center" />
                 <NextLink href="/blog" passHref>
                     <Link
                         fontSize="md"
