76c99271150de43d22c45686794efd78aeaa5685
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-16 13:36:52 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,14 +8,21 @@ export default class MyDocument extends NextDocument {
             <Html lang="en">
                 <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" />
                 <Head>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q482QX788Z"></script>
-                    <script>
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
+                    <script
+                      async
+                      src="https://www.googletagmanager.com/gtag/js?id=G-Q482QX788Z"
+                    />
 
-                      gtag('config', 'G-Q482QX788Z');
-                    </script>
+                    <script
+                      dangerouslySetInnerHTML={{
+                        __html: `
+                          window.dataLayer = window.dataLayer || [];
+                          function gtag(){dataLayer.push(arguments);}
+                          gtag('js', new Date());
+                          gtag('config', '[Tracking ID]', { page_path: window.location.pathname });
+                        `,
+                      }}
+                    />
                 <Head />
                 <body>
                     <ColorModeScript />
