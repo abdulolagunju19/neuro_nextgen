4eb447573f03b567e06fce841ab3a0cfbf06a43c
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-16 13:29:12 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,16 @@ export default class MyDocument extends NextDocument {
         return (
             <Html lang="en">
                 <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" />
+                <Head>
+                    <!-- Global site tag (gtag.js) - Google Analytics -->
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q482QX788Z"></script>
+                    <script>
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', 'G-Q482QX788Z');
+                    </script>
                 <Head />
                 <body>
                     <ColorModeScript />
