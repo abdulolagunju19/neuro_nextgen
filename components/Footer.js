8c8565ae5b4013988ada033f00dbdba70c04f6d9
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-24 13:15:45 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -19,45 +19,47 @@ export const Footer = () => {
         <Flex align="center" mb={4} direction="column" visibility={'hidden', 'visible', 'visible'} display={['none', 'flex', 'flex']}>
             <Divider mb={5} mt={3} w='50%' alignSelf="center" />
             <div>
-                <NextLink href="/blog" passHref>
-                    <Button>
-                        <Link
-                            fontSize="md"
-                            color="gray.500"
-                            minWidth="100px"
-                            mr={2}
-                            title="Blog"
-                        >
-                           My Blog 
-                        </Link>
-                    </Button>
-                </NextLink>
-                <NextLink href="/simplestats" passHref>
-                    <Button>
-                        <Link
-                            fontSize="md"
-                            color="gray.500"
-                            minWidth="100px"
-                            mr={2}
-                            title="Simplistic Statistics"
-                        >
-                             | Tutorials |
-                        </Link>
-                    </Button>
-                </NextLink>
-                <NextLink href="/contactme" passHref>
-                    <Button>
-                        <Link
-                            fontSize="md"
-                            color="gray.500"
-                            minWidth="100px"
-                            mr={2}
-                            title="Contact Me"
-                        >
-                            Contact Me
-                        </Link>
-                    </Button>
-                </NextLink>
+                <Stack spacing={4} direction="row" align="center">
+                    <NextLink href="/blog" passHref>
+                        <Button>
+                            <Link
+                                fontSize="md"
+                                color="gray.500"
+                                minWidth="100px"
+                                mr={2}
+                                title="Blog"
+                            >
+                               My Blog 
+                            </Link>
+                        </Button>
+                    </NextLink>
+                    <NextLink href="/simplestats" passHref>
+                        <Button>
+                            <Link
+                                fontSize="md"
+                                color="gray.500"
+                                minWidth="100px"
+                                mr={2}
+                                title="Simplistic Statistics"
+                            >
+                                 Tutorials
+                            </Link>
+                        </Button>
+                    </NextLink>
+                    <NextLink href="/contactme" passHref>
+                        <Button>
+                            <Link
+                                fontSize="md"
+                                color="gray.500"
+                                minWidth="100px"
+                                mr={2}
+                                title="Contact Me"
+                            >
+                                Contact Me
+                            </Link>
+                        </Button>
+                    </NextLink>
+                </Stack>
             </div>
         </Flex>
     )
