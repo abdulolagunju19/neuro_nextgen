507b96c8c6a693dafe02fdf58f3548f2b91c3d47
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-24 13:11:22 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import NextLink from 'next/link'
 import { Flex, Link, IconButton, useColorMode } from '@chakra-ui/react'
-import { Divider } from "@chakra-ui/react"
+import { Divider, Box, Button } from "@chakra-ui/react"
 
 
 export const Footer = () => {
@@ -20,37 +20,43 @@ export const Footer = () => {
             <Divider mb={5} mt={3} w='50%' alignSelf="center" />
             <div>
                 <NextLink href="/blog" passHref>
-                    <Link
-                        fontSize="md"
-                        color="gray.500"
-                        minWidth="100px"
-                        mr={2}
-                        title="Blog"
-                    >
-                       My Blog 
-                    </Link>
+                    <Button>
+                        <Link
+                            fontSize="md"
+                            color="gray.500"
+                            minWidth="100px"
+                            mr={2}
+                            title="Blog"
+                        >
+                           My Blog 
+                        </Link>
+                    </Button>
                 </NextLink>
                 <NextLink href="/simplestats" passHref>
-                    <Link
-                        fontSize="md"
-                        color="gray.500"
-                        minWidth="100px"
-                        mr={2}
-                        title="Simplistic Statistics"
-                    >
-                         | Tutorials |
-                    </Link>
+                    <Button>
+                        <Link
+                            fontSize="md"
+                            color="gray.500"
+                            minWidth="100px"
+                            mr={2}
+                            title="Simplistic Statistics"
+                        >
+                             | Tutorials |
+                        </Link>
+                    </Button>
                 </NextLink>
                 <NextLink href="/contactme" passHref>
-                    <Link
-                        fontSize="md"
-                        color="gray.500"
-                        minWidth="100px"
-                        mr={2}
-                        title="Contact Me"
-                    >
-                        Contact Me
-                    </Link>
+                    <Button>
+                        <Link
+                            fontSize="md"
+                            color="gray.500"
+                            minWidth="100px"
+                            mr={2}
+                            title="Contact Me"
+                        >
+                            Contact Me
+                        </Link>
+                    </Button>
                 </NextLink>
             </div>
         </Flex>
