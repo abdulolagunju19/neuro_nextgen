c2c99356ec13341312f5808f99f20cc748f915cf
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-26 14:34:41 -0600

    Update Subscribe.js

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -47,12 +47,18 @@ export const Subscribe = () => {
 
       return;
     }
-
+    const toast = useToast()
     // 5. Clear the input value and show a success message.
     inputEl.current.value = '';
-    setMessage('Success! 🎉 You are now subscribed to my posts.');
+    useEffect(() => { toast({
+        title: "Account added.",
+        description: "Your email has been added to the subscription list.",
+        status: "success",
+        duration: 3500,
+        isClosable: true,
+   })}
   };
-  const toast = useToast()
+  
   
   return (
       <Box mt='10' width='100%' borderRadius="lg" border="2px" borderColor="gray.200" p={8}>
@@ -72,15 +78,6 @@ export const Subscribe = () => {
               />
               <Box mt='5' width='full'>
               <Button type="submit"
-                onClick={() =>
-                toast({
-                  title: "Account added.",
-                  description: "Your email has been added to the subscription list.",
-                  status: "success",
-                  duration: 3500,
-                  isClosable: true,
-                })
-                }
                 >{'✨ Subscribe 💌'}
               </Button>
               </Box>
