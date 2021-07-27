95183b3de953926f75e11248ebb35fe3bc2654b6
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-27 14:05:22 -0600

    Update Subscribe.js

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -18,68 +18,6 @@ import {
 } from "@chakra-ui/react"
 import { Formik } from 'formik';
 import { useToast } from "@chakra-ui/react"
-import ReactCanvasConfetti from 'react-canvas-confetti';
-
-const canvasStyles = {
-  position: 'fixed',
-  pointerEvents: 'none',
-  width: '100%',
-  height: '100%',
-  top: 0,
-  left: 0
-}
-
-export default class Realistic extends React.Component {
-  constructor(props) {
-    super(props);
-    this.animationInstance = null;
-  }
-
-  makeShot = (particleRatio, opts) => {
-    this.animationInstance && this.animationInstance({
-      ...opts,
-      origin: { y: 0.7 },
-      particleCount: Math.floor(200 * particleRatio),
-    });
-  }
-
-  fire = () => {
-    this.makeShot(0.25, {
-      spread: 26,
-      startVelocity: 55,
-    });
-
-    this.makeShot(0.2, {
-      spread: 60,
-    });
-
-    this.makeShot(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8,
-    });
-
-    this.makeShot(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2,
-    });
-
-    this.makeShot(0.1, {
-      spread: 120,
-      startVelocity: 45,
-    });
-  }
-
-  handlerFire = () => {
-    this.fire();
-  };
-
-  getInstance = (instance) => {
-    this.animationInstance = instance;
-  };
-
 
 export const Subscribe = () => {
   // 1. Create a reference to the input so we can fetch/clear it's value.
@@ -140,9 +78,7 @@ export const Subscribe = () => {
                   status: "success",
                   duration: 3500,
                   isClosable: true,
-                });
-                onClick={this.handlerFire};
-                }
+                })
                 >{'✨ Subscribe 💌'}
               </Button>
               </Box>
