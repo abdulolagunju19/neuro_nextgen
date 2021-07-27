c7eacb98b97bc863282b440179ad89f5d251f824
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-27 15:41:09 -0600

    Create next-seo.config.js

---
+++ next-seo.config.js
@@ -0,0 +1,17 @@
+const title = 'Abdul-Samad Olagunju –> An avid learner who wants to educate others.'
+const description =
+    'The website of Abdul-Samad Olagunju, a student at the University of Alberta.'
+
+const SEO = {
+    title,
+    description,
+    canonical: 'https://abneuro.vercel.app/',
+    openGraph: {
+        type: 'website',
+        url: 'https://abneuro.vercel.app/',
+        title,
+        description,
+    }
+}
+
+export default SEO
