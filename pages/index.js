5d8c578cf77599a4a9a4ac62ce4b9f4a8f96c1c4
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-31 20:42:06 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ import {
 } from '@chakra-ui/react'
 
 import Container from '../components/Container'
+import { NextSeo } from 'next-seo'
+
+const url = 'https://abneuro.vercel.app/'
+const title = 'Home Page – Abdul-Samad Olagunju'
+const description = 'The Personal Website of Abdul-Samad Olagunju.'
 
 export default function Index() {
   const { colorMode } = useColorMode()
@@ -20,44 +25,56 @@ export default function Index() {
     dark: 'gray.400'
   }
   return (
-    <Container>
-      <Head>
-        <title>Home - Abdul-Samad Olagunju</title>
-      </Head>
-      <Stack
-        as="main"
-        spacing={8}
-        justifyContent="center"
-        alignItems="flex-start"
-        m="0 auto 4rem auto"
-        maxWidth="700px"
-        px={2}
-      >
-        <Flex
-          flexDirection="column"
-          justifyContent="flex-start"
+    <>
+      <NextSeo
+          title={title}
+          description={description}
+          canonical={url}
+          openGraph={{
+            url,
+            title,
+            description
+          }}
+        />
+      <Container>
+        <Head>
+          <title>Home - Abdul-Samad Olagunju</title>
+        </Head>
+        <Stack
+          as="main"
+          spacing={8}
+          justifyContent="center"
           alignItems="flex-start"
+          m="0 auto 4rem auto"
           maxWidth="700px"
-          className="animate__animated animate__fadeInLeft"
+          px={2}
         >
-          <Heading mb={2}>Hi, I'm Abdul-Samad Olagunju</Heading>
-          <br />
-          <Text color={colorSecondary[colorMode]}> I'm a Neuroscience student at the University of Alberta. This is my personal website, where I write about statistics, neuroscience, and display my projects. I created this website because I believe that complicated ideas should be understandable to the general public. I want to share everything that I have learned, and I hope that it helps you. </Text>
-          <br />
-          <Text color={colorSecondary[colorMode]}> I want to thank Benjamin Carlson for his personal website tutorial, it helped me get my feet off the ground in web development. </Text>
-          <br />
-          <div>
-            <Image
-              src="/images/westbrook.gif"
-              alt="Photo"
-              width={900}
-              height={900}
-              priority
-              className="next-image"
-            />
-          </div>
-        </Flex>
-      </Stack>
-    </Container>
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            maxWidth="700px"
+            className="animate__animated animate__fadeInLeft"
+          >
+            <Heading mb={2}>Hi, I'm Abdul-Samad Olagunju</Heading>
+            <br />
+            <Text color={colorSecondary[colorMode]}> I'm a Neuroscience student at the University of Alberta. This is my personal website, where I write about statistics, neuroscience, and display my projects. I created this website because I believe that complicated ideas should be understandable to the general public. I want to share everything that I have learned, and I hope that it helps you. </Text>
+            <br />
+            <Text color={colorSecondary[colorMode]}> I want to thank Benjamin Carlson for his personal website tutorial, it helped me get my feet off the ground in web development. </Text>
+            <br />
+            <div>
+              <Image
+                src="/images/westbrook.gif"
+                alt="Photo"
+                width={900}
+                height={900}
+                priority
+                className="next-image"
+              />
+            </div>
+          </Flex>
+        </Stack>
+      </Container>
+    </>
   )
 }
