9f7caa09450b1dd42fe0f0afa79f8a69fc1708dc
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-07-31 20:45:34 -0600

    Update contactme.js

--- pages/contactme.js
+++ pages/contactme.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import "animate.css"
+import { NextSeo } from 'next-seo'
 import {
   useColorMode,
   Heading,
@@ -14,6 +15,10 @@ import {
 import Container from '../components/Container'
 import { Subscribe } from '../components/Subscribe';
 
+const url = 'https://abneuro.vercel.app/simplestats'
+const title = 'Contact Me – Abdul-Samad Olagunju'
+const description = 'Contact Me if you have any Further Questions.'
+
 export default function ContactMe() {
   const { colorMode } = useColorMode()
   const colorSecondary = {
@@ -21,40 +26,52 @@ export default function ContactMe() {
     dark: 'gray.400'
   }
   return (
-    <Container>
-      <Head>
-        <title>Want to connect further?</title>
-      </Head>
-      <Stack
-        as="main"
-        spacing={8}
-        justifyContent="center"
-        alignItems="flex-start"
-        m="0 auto 4rem auto"
-        maxWidth="700px"
-        px={2}
-      >
-        <Flex
-          flexDirection="column"
-          justifyContent="flex-start"
+    <>
+      <NextSeo
+          title={title}
+          description={description}
+          canonical={url}
+          openGraph={{
+            url,
+            title,
+            description
+          }}
+        />
+      <Container>
+        <Head>
+          <title>Want to connect further?</title>
+        </Head>
+        <Stack
+          as="main"
+          spacing={8}
+          justifyContent="center"
           alignItems="flex-start"
+          m="0 auto 4rem auto"
           maxWidth="700px"
-          className="animate__animated animate__fadeInLeft"
+          px={2}
         >
-          <Heading mb={2}>Want to connect further?</Heading>
-          <Subscribe/>
-          <br />
-          <Text color={colorSecondary[colorMode]} fontSize="xl" fontWeight="semibold"> You can also reach me at this email address: olagunju@ualberta.ca </Text>
-        </Flex>
-      </Stack>
-      <VStack spacing="25px">
-          <Text fontSize="2xl" as="ins">Click the picture to see a cool effect!</Text>
-          <Text fontSize="3xl">&darr;</Text>
-          <Box borderRadius="lg" overflow="hidden" p={5} w ="51%" h ="650">
-            <iframe width="300%" height="800px" src="https://preview.p5js.org/abdulolagunju19/embed/PYvsqEjBM"></iframe>
-          </Box>
-      </VStack>
-    </Container>
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            maxWidth="700px"
+            className="animate__animated animate__fadeInLeft"
+          >
+            <Heading mb={2}>Want to connect further?</Heading>
+            <Subscribe/>
+            <br />
+            <Text color={colorSecondary[colorMode]} fontSize="xl" fontWeight="semibold"> You can also reach me at this email address: olagunju@ualberta.ca </Text>
+          </Flex>
+        </Stack>
+        <VStack spacing="25px">
+            <Text fontSize="2xl" as="ins">Click the picture to see a cool effect!</Text>
+            <Text fontSize="3xl">&darr;</Text>
+            <Box borderRadius="lg" overflow="hidden" p={5} w ="51%" h ="650">
+              <iframe width="300%" height="800px" src="https://preview.p5js.org/abdulolagunju19/embed/PYvsqEjBM"></iframe>
+            </Box>
+        </VStack>
+      </Container>
+    </>
   )
 }
 
