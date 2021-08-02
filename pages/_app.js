b5ec2e9356857d1cebe8a57babd4701024fbe721
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-08-02 14:47:39 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,6 @@ import { prismLightTheme, prismDarkTheme } from '../styles/prism'
 import SEO from '../next-seo.config';
 import { DefaultSeo } from 'next-seo'
 
-import Router from 'next/router'
-import ProgressBar from "@badrap/bar-of-progress";
-
-const progress = new ProgressBar({
-  size: 2,
-  color: "#29e",
-  className: "bar-of-progress",
-  delay: 100,
-});
-
-Router.events.on("routeChangeStart", progress.start);
-Router.events.on("routeChangeComplete", progress.finish);
-Router.events.on("routeChangeError", progress.finish);
-
 const GlobalStyle = ({ children }) => {
   const { colorMode } = useColorMode()
 
