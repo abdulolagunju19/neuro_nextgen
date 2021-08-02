09f83a6490fa7a4fd37f991cfa2cea9ef4eaeaa6
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-08-02 14:44:08 -0600

    Update _app_g.js

--- pages/_app_g.js
+++ pages/_app_g.js
@@ -2,6 +2,20 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
 
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from 'next/router'
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#29e",
+  className: "bar-of-progress",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
+
 const App = ({ Component, pageProps }) => {
   const router = useRouter()
   useEffect(() => {
