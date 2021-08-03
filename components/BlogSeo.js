46e5a6da279c25323fea1b52c94ac0e162ba22e0
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-08-03 10:56:20 -0600

    Create BlogSeo.js

---
+++ components/BlogSeo.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { NextSeo, ArticleJsonLd } from 'next-seo'
+
+const BlogSeo = ({ title, summary, publishedAt, url, image }) => {
+    const date = new Date(publishedAt).toISOString()
+
+    return (
+        <>
+            <NextSeo
+                title={`${title} – Abdul-Samad Olagunju`}
+                description={summary}
+                canonical={url}
+                openGraph={{
+                    type: 'article',
+                    article: {
+                        publishedTime: date
+                    },
+                    url,
+                    title,
+                    description: summary
+                }}
+            />
+            <ArticleJsonLd
+                authorName="Abdul-Samad Olagunju"
+                dateModified={date}
+                datePublished={date}
+                description={summary}
+                publisherName="Abdul-Samad Olagunju"
+                title={title}
+                url={url}
+            />
+        </>
+    )
+}
+
+export default BlogSeo
