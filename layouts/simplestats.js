95110b1921fb66867856d66997e3ebf87cb2dfbb
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-08-04 13:30:52 -0600

    Update simplestats.js

--- layouts/simplestats.js
+++ layouts/simplestats.js
@@ -14,7 +14,6 @@ import { NextSeo } from 'next-seo'
 import { useRouter } from 'next/router'
 
 import Container from '../components/Container'
-import BlogSeo from '../components/BlogSeo'
 
 export default function SimpleStatsLayout({ children, frontMatter }) {
     const { colorMode } = useColorMode()
@@ -45,7 +44,7 @@ export default function SimpleStatsLayout({ children, frontMatter }) {
     return (
         <>
             <NextSeo
-                title={frontMatter.title}
+                title= {frontMatter.title}
                 description={frontMatter.summary}
                 openGraph={{
                 title: frontMatter.title,
@@ -55,7 +54,6 @@ export default function SimpleStatsLayout({ children, frontMatter }) {
             />
             <Box h={1} as="div" bgGradient="linear(to-b, blue.200, blue.500)" position="sticky" top={0} zIndex={100} w={`${width}%`}></Box>
             <Container>
-                <BlogSeo url={`https://abneuro.vercel.app/simplestats${slug}`} {...frontMatter} />
                 <Head>
                     <title>{frontMatter.title} - Make Statistics Easy - Abdul_Samad Olagunju</title>
                 </Head>
