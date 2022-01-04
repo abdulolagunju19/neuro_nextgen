86ad900c5abf9d40cf98ba50c6b215ef07d8e8b3
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2022-01-03 18:14:20 -0700

    Update contactme.js

--- pages/contactme.js
+++ pages/contactme.js
@@ -75,6 +75,11 @@ export default function ContactMe() {
             </Button>
             <br />
             <Text color={colorSecondary[colorMode]} fontSize="xl" fontWeight="semibold"> You can also reach me at this email address: olagunju@ualberta.ca </Text>
+            <Text fontSize="2xl" as="ins">As a send off, click the picture below to see a cool effect!</Text>
+            <Text fontSize="3xl">&darr;</Text>
+            <Box borderRadius="lg" overflow="hidden" p={5} w ="51%" h ="650">
+              <iframe width="300%" height="800px" src="https://preview.p5js.org/abdulolagunju19/embed/PYvsqEjBM"></iframe>
+            </Box>
         </VStack>
       </Container>
     </>
