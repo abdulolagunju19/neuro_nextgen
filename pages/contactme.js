fa3eab6a01d43ea5e7e9b287c765c361167b8df7
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2022-01-03 18:45:30 -0700

    Update contactme.js

--- pages/contactme.js
+++ pages/contactme.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import "animate.css"
 import { NextSeo } from 'next-seo'
+import { useMediaQuery } from 'react-responsive'
 import {
   useColorMode,
   Heading,
@@ -28,6 +29,15 @@ export default function ContactMe() {
     dark: 'gray.400'
   }
   const router = useRouter()
+
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  
+  const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
   return (
     <>
       <NextSeo
@@ -63,8 +73,8 @@ export default function ContactMe() {
             <Heading mb={2}>Want to connect further?</Heading>
             <Subscribe/>
           </Flex>
-        </Stack>
-        <VStack spacing="15px" p="4">
+        </Stack> 
+          <VStack spacing="15px" p="4">
             <Button
             variant="solid"
             size="lg"
@@ -74,13 +84,27 @@ export default function ContactMe() {
               Get in touch!
             </Button>
             <br />
-            <Text color={colorSecondary[colorMode]} fontSize="xl" fontWeight="semibold"> You can also reach me at this email address: olagunju@ualberta.ca </Text>
-            <Text fontSize="2xl" as="ins">As a send off, click the picture below to see a cool effect!</Text>
-            <Text fontSize="3xl">&darr;</Text>
-            <Box borderRadius="lg" overflow="hidden" p={5} w ="51%" h ="650">
-              <iframe width="300%" height="800px" src="https://preview.p5js.org/abdulolagunju19/embed/PYvsqEjBM"></iframe>
-            </Box>
-        </VStack>
+            {isDesktopOrLaptop &&
+              <VStack>
+                <Text color={colorSecondary[colorMode]} fontSize="xl" fontWeight="semibold"> You can also reach me at this email address: olagunju@ualberta.ca </Text>
+                <Text color={colorSecondary[colorMode]} fontSize="2xl" fontWeight="semibold">As a send off, click the picture below to see a cool effect!</Text>
+                <Text color={colorSecondary[colorMode]} fontSize="2xl" fontWeight="semibold">&darr;</Text>
+                <Box borderRadius="lg" overflow="hidden" p={5} w ="100%" h ="650">
+                  <iframe width="300%" height="640px" src="https://preview.p5js.org/abdulolagunju19/embed/PYvsqEjBM"></iframe>
+                </Box>
+              </VStack>
+            }
+            {isTabletOrMobile &&
+              <VStack>
+                <Text color={colorSecondary[colorMode]} fontSize="md" fontWeight="semibold"> You can also reach me at this email address: olagunju@ualberta.ca </Text>
+                <Text color={colorSecondary[colorMode]} fontSize="md" fontWeight="semibold">As a send off, click the picture below to see a cool effect!</Text>
+                <Text color={colorSecondary[colorMode]} fontSize="md" fontWeight="semibold">&darr;</Text>
+                <Box borderRadius="lg" overflow="hidden" p={5} w ="70%" h ="150">
+                  <iframe width="300%" height="640px" src="https://preview.p5js.org/abdulolagunju19/embed/PYvsqEjBM"></iframe>
+                </Box>
+              </VStack>
+            }
+          </VStack>
       </Container>
     </>
   )
